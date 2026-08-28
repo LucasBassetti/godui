2c53d6f621d52ed574738a5caf93aeecdd32cd12
Joseph Heitman <joecheitman@gmail.com>
2026-08-27 20:37:21 -0400

    fix(multi-button): preserve standard touch activation

--- packages/components/src/multi-button/multi-button.tsx
+++ packages/components/src/multi-button/multi-button.tsx
@@ -1216,7 +1216,6 @@ const MultiButton = React.forwardRef<HTMLDivElement, MultiButtonProps>(
     ) => {
       if (event.pointerType !== "touch") return;
       if (touchExpandedId !== id) {
-        event.preventDefault();
         setTouchExpandedId(id);
       }
     };
